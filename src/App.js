cf9ea5fcc94f310dde90625c396d1f07011b58e7
edoverschuur <edoverschuur@gmail.com>
2022-06-21 22:22:20 +0200

    merged

--- src/App.js
+++ src/App.js
@@ -26,29 +26,48 @@ axios.interceptors.request.use(function (config) {
 
 class App extends React.Component{
 
-    render () {
-        return (
-            
-            <main className="App">
-            <Navbar />
-                <Router>
-                    <Switch>
-                        <Route exact path="/home" component={Home}/>
-                        <Route exact path="/profiel" component={Profile}/>
+    state = {page: "home"}
 
-                        <Route exact path="/scoreboard" component={Scoreboard}/>
+    ffChecken = bloeb => {
+        const BASE_URL = "localhost:8000/api/exercise/";
+    };
+    
+    toMessages = () => {
+        this.setState({page:'notifications'})
+    }
 
-                        <Route path="/register">
-                            {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Register />}
-                        </Route>
-                        <Route path="/login">
-                            {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Login />}
-                        </Route>
-                        
-                    </Switch>
-                </Router>
-            </main>
-        )
+    render(){
+        if (this.state.page == "home"){
+            return (
+                <main>
+                    <Navbar />
+                    <Home navigation = {this.toMessages}/>
+                    {/* <Aanmeld onSubmit={this.ffChecken} /> */}
+                    <Router>
+                        <Switch>
+                            <Route exact path="/profiel" component={Profile}/>
+
+                            <Route path="/register">
+                                {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Register />}
+                            </Route>
+                            <Route path="/login">
+                                {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Login />}
+                            </Route>
+                            
+                        </Switch>
+                    </Router>
+                </main>
+            );
+        }else if(this.state.page == "notifications"){
+            return (
+                <article>
+                    <Navbar />
+                    <Notifications />
+                    
+                </article>
+            );
+        }
+        
     }
 }
 
