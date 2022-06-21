347b2f4a1775d0ac907653ac756914da2ef3dc8e
edoverschuur <edoverschuur@gmail.com>
2022-06-21 12:16:08 +0200

    added logout

--- src/App.js
+++ src/App.js
@@ -11,13 +11,21 @@ import Navbar from "./components/Navbar";
 axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
+
 axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('auth_token');
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+    return config;
+});
 
 class App extends React.Component{
 
     render () {
         return (
+            
             <main className="App">
+            <Navbar />
                 <Router>
                     <Switch>
                         <Route exact path="/register" component={Register}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,31 @@
+import { getActiveElement } from "@testing-library/user-event/dist/utils";
+import axios from "axios";
 import React from "react";
 import "./Navbar.css";
 
 class Navbar extends React.Component{
-  
+
+    logoutSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post(`/api/logout`).then(res => {
+            if (res.data.status === 200) {
+                localStorage.removeItem('auth_token', res.data.token);
+                localStorage.removeItem('auth_name', res.data.username);
+                window.location.replace("/");
+            } else {
+
+            }
+        });
+    }
     
-    render(){
-        /* If ingelogd, dan eerste return, anders tweede*/
-        console.log("navbar ingeladen");
-        if(false){
+    render () {
+        if (!localStorage.getItem('auth_token')) {
             return (
                 <nav class="nav">
                     <a class="nav__a" href="/home"><p class="nav__a__p">Home</p></a>
-                    <a class="nav__a" href="/register"><p class="nav__a__p">Aanmelden</p></a>
                     <a class="nav__a" href="/scoreboard"><p class="nav__a__p">Scoreboard</p></a>
+                    <a class="nav__a" href="/register"><p class="nav__a__p">Aanmelden</p></a>
                     <a class="nav__a" href="/login"><p class="nav__a__p">Login</p></a>
                 </nav>
             )
@@ -20,8 +33,9 @@ class Navbar extends React.Component{
             return (
                 <nav class="nav">
                     <a class="nav__a" href="/home"><p class="nav__a__p">Home</p></a>
-                    <a class="nav__a" href="#contact"><p class="nav__a__p">Contact</p></a>
+                    <a class="nav__a" href="/scoreboard"><p class="nav__a__p">Scoreboard</p></a>
                     <a class="nav__a" href="/profiel"><p class="nav__a__p">Profiel</p></a>
+                    <a class="nav__a" href="/logout" onClick={this.logoutSubmit}><p class="nav__a__p">Logout</p></a>
                 </nav>
             )
         }
