c03c7de8ea89aa5fe27859f20788229c2439f631
KoenSmit <ksp.smit@gmail.com>
2022-06-23 16:42:33 +0200

    minor style changes

--- src/Home.js
+++ src/Home.js
@@ -25,10 +25,10 @@ class Home extends React.Component{
                         <img className="section__info__img" src={sporter} alt="Foto van hardlopers." />
                     </figure>
                     <section className="section__info__text text1">
-                        <p>Je kent het wel. Je komt aan op je werk en je stapt richting het trappenhuis, maar in je ooghoek merk je op dat de lift toevallig net beneden aankomt. Ach, hij staat er nou toch al! Eenmaal op de 3e verdieping ga je zitten aan je bureau, nadat je net nog een koffie even hebt gepakt. Dat is het dan. Dat was je beweging voor de ochtend. De aankomende 8 uur zit je op je reet en zet je je lichaam in een 90 graden hoek op een stoel met wieltjes.</p>
+                        <p className="section__p">Je kent het wel. Je komt aan op je werk en je stapt richting het trappenhuis, maar in je ooghoek merk je op dat de lift toevallig net beneden aankomt. Ach, hij staat er nou toch al! Eenmaal op de 3e verdieping ga je zitten aan je bureau, nadat je net nog een koffie even hebt gepakt. Dat is het dan. Dat was je beweging voor de ochtend. De aankomende 8 uur zit je op je reet en zet je je lichaam in een 90 graden hoek op een stoel met wieltjes.</p>
                     </section>
                     <section className="section__info__text text2">
-                        <p>Dit kan natuurlijk niet langer! Steeds meer mensen werken op kantoor en bewegen daardoor te weinig. Daarom is er nu het nationale beweegminuutje! Iedereen kan zich aanmelden en meedoen aan gezonder Nederland! Het werkt heel simpel: Je meldt je aan via deze website en daarna krijg je elke werkdag 1x een melding wanneer je moet sporten. Doe snel mee!</p>
+                        <p className="section__p">Dit kan natuurlijk niet langer! Steeds meer mensen werken op kantoor en bewegen daardoor te weinig. Daarom is er nu het nationale beweegminuutje! Iedereen kan zich aanmelden en meedoen aan gezonder Nederland! Het werkt heel simpel: Je meldt je aan via deze website en daarna krijg je elke werkdag 1x een melding wanneer je moet sporten. Doe snel mee!</p>
                     </section>
                     <figure className="section__info__figure figure2">
                         <img className="section__info__img" src={kantoor} alt="Plaatje" />
